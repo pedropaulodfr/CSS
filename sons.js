e65a4be4a942f5c47fbbf0989991d0f6716e8be9
pedropaulodfr <pedropaulodfr@outlook.com>
2017-08-18 14:45:36 -0300

    Create sons.js

---
+++ sons.js
@@ -0,0 +1,258 @@
+	function intializePlayer(){
+	document.getElementById('splash').style.display = 'none';
+	document.getElementById('chimbal_pedal').style.display = 'none';
+	document.getElementById('chimbal_pedal_2').style.display = 'none';
+	document.getElementById('pedal_2').style.display = 'none';
+	document.getElementById('chimbal').style.display = 'none';
+	}
+	window.onload = intializePlayer;
+	function chimbal(){
+	document.getElementById('chimbal_som').src = "https://sites.google.com/site/pedropaulodantasfrancorocha/hospedagem/chimbal.ogg";
+	document.getElementById('chimbal_pedal').style.display = 'none';
+	document.getElementById('chimbal_pedal_2').style.display = 'none';
+	document.getElementById('chimbal').style.display = 'block';
+	}
+	function chimbal_pedal(){
+	document.getElementById('chimbal_pedal_som').src = "https://sites.google.com/site/pedropaulodantasfrancorocha/hospedagem/chimbal_pedal.ogg";
+	document.getElementById('chimbal_pedal').style.display = 'block';
+	document.getElementById('chimbal_pedal_2').style.display = 'block';
+	document.getElementById('chimbal').style.display = 'none';
+	}
+	function caixa(){
+	document.getElementById('caixa_som').src = "https://sites.google.com/site/pedropaulodantasfrancorocha/hospedagem/caixa.ogg";
+	document.getElementById('pele_caixa').style.display = 'block';
+	}
+	function aro(){
+	document.getElementById('aro_som').src = "https://sites.google.com/site/pedropaulodantasfrancorocha/hospedagem/aro.ogg?attredirects=0&d=1";
+	}
+	function bumbo(){
+	document.getElementById('bumbo_som').src = "https://sites.google.com/site/pedropaulodantasfrancorocha/hospedagem/bumbo.ogg";
+	document.getElementById('pedal_2').style.display = 'block';
+	document.getElementById('pedal_1').style.display = 'none';
+	document.getElementById('pele_bumbo').style.display = 'block';
+	}
+	function tom_1(){
+	document.getElementById('tom_1_som').src = "https://sites.google.com/site/pedropaulodantasfrancorocha/hospedagem/tom_1.ogg";
+	document.getElementById('pele_tom_1').style.display = 'block';
+	}
+	function tom_2(){
+	document.getElementById('tom_2_som').src = "https://sites.google.com/site/pedropaulodantasfrancorocha/hospedagem/tom_2.ogg";
+	document.getElementById('pele_tom_2').style.display = 'block';
+	}
+	function tom_3(){
+	document.getElementById('tom_3_som').src = "https://sites.google.com/site/pedropaulodantasfrancorocha/hospedagem/tom_3.ogg";
+	document.getElementById('pele_tom_3').style.display = 'block';
+	}
+	function surdo_1(){
+	document.getElementById('surdo_1_som').src = "https://sites.google.com/site/pedropaulodantasfrancorocha/hospedagem/surdo_1.ogg";
+	document.getElementById('surdo_1').style.display = 'block';
+	}
+	function surdo_2(){
+	document.getElementById('surdo_2_som').src = "https://sites.google.com/site/pedropaulodantasfrancorocha/hospedagem/surdo_2.ogg";
+	document.getElementById('surdo_2').style.display = 'block';
+	}
+	function prato_1(){
+	document.getElementById('prato_1_som').src = "https://sites.google.com/site/pedropaulodantasfrancorocha/hospedagem/prato_1.ogg";
+	document.getElementById('prato_1').style.display = 'block';
+	}
+	function prato_2(){
+	document.getElementById('prato_2_som').src = "https://sites.google.com/site/pedropaulodantasfrancorocha/hospedagem/prato_2.ogg";
+	document.getElementById('prato_2').style.display = 'block';
+	}
+	function splash(){
+	document.getElementById('splash_som').src = "https://sites.google.com/site/pedropaulodantasfrancorocha/hospedagem/splash.ogg";
+	document.getElementById('splash').style.display = 'block';
+	}
+	function conducao_1(){
+	document.getElementById('conducao_1_som').src = "https://sites.google.com/site/pedropaulodantasfrancorocha/hospedagem/conducao_1.ogg";
+	document.getElementById('conducao_1').style.display = 'block';
+	}
+	function conducao_2(){
+	document.getElementById('conducao_2_som').src = "https://sites.google.com/site/pedropaulodantasfrancorocha/hospedagem/conducao_2.ogg";
+	document.getElementById('conducao_2').style.display = 'block';
+	}
+	function baqueta(){
+	document.getElementById('baqueta_som').src = "https://sites.google.com/site/pedropaulodantasfrancorocha/hospedagem/Baqueta.ogg?attredirects=0&d=1";
+	}
+	/*----------------------------------*/
+	
+	function chimbal2(){
+	document.getElementById('chimbal_som').src = "https://sites.google.com/site/pedropaulodantasfrancorocha/bateria2/chimbal.ogg";
+	document.getElementById('chimbal_pedal').style.display = 'none';
+	document.getElementById('chimbal_pedal_2').style.display = 'none';
+	document.getElementById('chimbal').style.display = 'block';
+	}
+	function chimbal_pedal_2(){
+	document.getElementById('chimbal_pedal_som').src = "https://sites.google.com/site/pedropaulodantasfrancorocha/bateria2/chimbal_pedal.ogg";
+	document.getElementById('chimbal_pedal').style.display = 'block';
+	document.getElementById('chimbal_pedal_2').style.display = 'block';
+	document.getElementById('chimbal').style.display = 'none';
+	}
+	function caixa2(){
+	document.getElementById('caixa_som').src = "https://sites.google.com/site/pedropaulodantasfrancorocha/bateria2/caixa.ogg";
+	document.getElementById('pele_caixa').style.display = 'block';
+	}
+	function aro2(){
+	document.getElementById('aro_som').src = "https://sites.google.com/site/pedropaulodantasfrancorocha/hospedagem/aro.ogg";
+	}
+	function bumbo2(){
+	document.getElementById('bumbo_som').src = "https://sites.google.com/site/pedropaulodantasfrancorocha/bateria2/bumbo.ogg";
+	document.getElementById('pedal_2').style.display = 'block';
+	document.getElementById('pedal_1').style.display = 'none';
+	document.getElementById('pele_bumbo').style.display = 'block';
+	}
+	function tom_1_2(){
+	document.getElementById('tom_1_som').src = "https://sites.google.com/site/pedropaulodantasfrancorocha/bateria2/tom_1.ogg";
+	document.getElementById('pele_tom_1').style.display = 'block';
+	}
+	function tom_2_2(){
+	document.getElementById('tom_2_som').src = "https://sites.google.com/site/pedropaulodantasfrancorocha/bateria2/tom_2.ogg";
+	document.getElementById('pele_tom_2').style.display = 'block';
+	}
+	function tom_3_2(){
+	document.getElementById('tom_3_som').src = "https://sites.google.com/site/pedropaulodantasfrancorocha/bateria2/tom_3.ogg";
+	document.getElementById('pele_tom_3').style.display = 'block';
+	}
+	function surdo_1_2(){
+	document.getElementById('surdo_1_som').src = "https://sites.google.com/site/pedropaulodantasfrancorocha/bateria2/surdo_1.ogg";
+	document.getElementById('surdo_1').style.display = 'block';
+	}
+	function surdo_2_2(){
+	document.getElementById('surdo_2_som').src = "https://sites.google.com/site/pedropaulodantasfrancorocha/bateria2/surdo_2.ogg";
+	document.getElementById('surdo_2').style.display = 'block';
+	}
+	function prato_1_2(){
+	document.getElementById('prato_1_som').src = "https://sites.google.com/site/pedropaulodantasfrancorocha/bateria2/prato_2.ogg";
+	document.getElementById('prato_1').style.display = 'block';
+	}
+	function prato_2_2(){
+	document.getElementById('prato_2_som').src = "https://sites.google.com/site/pedropaulodantasfrancorocha/bateria2/prato_1.ogg";
+	document.getElementById('prato_2').style.display = 'block';
+	}
+	function splash2(){
+	document.getElementById('splash_som').src = "https://sites.google.com/site/pedropaulodantasfrancorocha/bateria2/splash.ogg";
+	document.getElementById('splash').style.display = 'block';
+	}
+	function conducao_1_2(){
+	document.getElementById('conducao_1_som').src = "https://sites.google.com/site/pedropaulodantasfrancorocha/bateria2/conducao_1.ogg";
+	document.getElementById('conducao_1').style.display = 'block';
+	}
+	function conducao_2_2(){
+	document.getElementById('conducao_2_som').src = "https://sites.google.com/site/pedropaulodantasfrancorocha/bateria2/conducao_2.ogg";
+	document.getElementById('conducao_2').style.display = 'block';
+	}
+	
+	function tecla(){
+	var resul = event.keyCode;
+		if((resul == 99) || (resul == 118)){
+		caixa();
+		}
+		if(resul == 120){
+		aro();
+		}		
+		if(resul == 110){
+		chimbal();
+		}
+		if(resul == 98){
+		bumbo();
+		}
+		if((resul == 97) || (resul == 115)){
+		tom_1();
+		}
+		if((resul == 100) || (resul == 102)){
+		tom_2();
+		}
+		if((resul == 103) || (resul == 104)){
+		tom_3();
+		}
+		if((resul == 106) || (resul == 107)){
+		surdo_1();
+		}
+		if((resul == 108) || (resul == 231)){
+		surdo_2();
+		}
+		if(resul == 114){
+		prato_1();
+		}
+		if(resul == 116){
+		prato_2();
+		}
+		if(resul == 117){
+		conducao_1();
+		}
+		if(resul == 121){
+		conducao_2();
+		}
+		if(resul == 105){
+		splash();
+		}
+		if(resul == 109){
+		chimbal_pedal();
+		}
+		if(resul == 32){
+		baqueta();
+		}
+		/*----------------------------------*/
+		if((resul == 67) || (resul == 86)){
+		caixa2();
+		}
+		if(resul == 88){
+		aro2();
+		}		
+		if(resul == 78){
+		chimbal2();
+		}
+		if(resul == 66){
+		bumbo2();
+		}
+		if((resul == 65) || (resul == 83)){
+		tom_1_2();
+		}
+		if((resul == 68) || (resul == 70)){
+		tom_2_2();
+		}
+		if((resul == 71) || (resul == 72)){
+		tom_3_2();
+		}
+		if((resul == 74) || (resul == 75)){
+		surdo_1_2();
+		}
+		if((resul == 76) || (resul == 199)){
+		surdo_2_2();
+		}
+		if(resul == 82){
+		prato_1_2();
+		}
+		if(resul == 84){
+		prato_2_2();
+		}
+		if(resul == 85){
+		conducao_1_2();
+		}
+		if(resul == 89){
+		conducao_2_2();
+		}
+		if(resul == 73){
+		splash2();
+		}
+		if(resul == 77){
+		chimbal_pedal_2();
+		}
+	}
+	function efeito(){
+	document.getElementById('splash').style.display = 'none';
+	document.getElementById('pele_bumbo').style.display = 'none';
+	document.getElementById('pele_caixa').style.display = 'none';
+	document.getElementById('pele_tom_1').style.display = 'none';
+	document.getElementById('pele_tom_2').style.display = 'none';
+	document.getElementById('pele_tom_3').style.display = 'none';
+	document.getElementById('surdo_1').style.display = 'none';
+	document.getElementById('surdo_2').style.display = 'none';
+	document.getElementById('prato_1').style.display = 'none';
+	document.getElementById('prato_2').style.display = 'none';
+	document.getElementById('conducao_1').style.display = 'none';
+	document.getElementById('conducao_2').style.display = 'none';
+	document.getElementById('pedal_2').style.display = 'none';
+	document.getElementById('pedal_1').style.display = 'block';
+	document.getElementById('chimbal').style.display = 'none';
+	}
